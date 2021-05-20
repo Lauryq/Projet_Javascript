e085d3ee91a9c586dcbecb509def481fd758cf9c
Oscar Millot <oscarmillot23@gmail.com>
2021-05-20 17:07:13 +0200

    Mise à jour

--- integration/script_fonction.js
+++ integration/script_fonction.js
@@ -1,9 +1,7 @@
 // Ajout du text page Fonction || Début
 
 let boutonFon1 = document.getElementById('bouton_fonction1');
-let boutonFon2 = document.getElementById('bouton_fonction2');
-let boutonFon3 = document.getElementById('bouton_fonction3');
-let boutonFon4 = document.getElementById('bouton_fonction4');
+
 
 boutonFon1.addEventListener('click', function() {
     p1.innerHTML = `Une fonction est une procédure JavaScript, un ensemble d'instructions effectuant une tâche ou calculant une valeur.<br>
